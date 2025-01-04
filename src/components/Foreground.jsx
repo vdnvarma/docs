3091b0d2c9c9abac21940dffe8d703ad1e0eebb8
vdnvarma <dedeepn23@gmail.com>
2025-01-04 18:25:13 +0530

    initial commit

---
+++ src/components/Foreground.jsx
@@ -0,0 +1,34 @@
+import React, { useRef } from 'react'
+import Card from './Card'
+
+function Foreground() {
+    
+    const ref = useRef(null);
+
+    const data = [
+        {desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+            filesize: ".9mb",
+            close : true,
+             tag: {isOpen:true , tagTitle: "Download Now", tagColor: "green"},
+        },
+        {desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+            filesize: ".9mb",
+            close : false,
+             tag: {isOpen:true, tagTitle: "Download Now", tagColor: "blue"},
+        },
+        {desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+            filesize: ".9mb",
+            close : true,
+             tag: {isOpen:false, tagTitle: "Download Now", tagColor: "green"},
+        },
+    ];
+  return (
+    <div ref={ref} className='fixed top-0 left-0 z-[3] w-full h-full flex gap-10 flex-wrap'>
+        {data.map((item, index) => (
+            <Card data={item} reference={ref} />
+        ))}
+    </div>
+  )
+}
+
+export default Foreground
